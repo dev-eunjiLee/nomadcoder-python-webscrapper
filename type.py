630bb0d26f97c6e25f57d65425a4bba460116d93
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-14 21:11:54 +0900

    [2.1] Lists in Python

--- type.py
+++ type.py
@@ -4,8 +4,18 @@ a_float = 3.12
 a_boolean = False
 a_none = None
 
+b_list = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+
 print(type(a_string))
 print(type(a_number))
 print(type(a_float))
 print(type(a_boolean))
 print(type(a_none))
+
+print(type(b_list), b_list, "Mon" in b_list, b_list[3], len(b_list))
+
+b_list.append("Sat")
+print(b_list)
+
+b_list.reverse()
+print(b_list)
