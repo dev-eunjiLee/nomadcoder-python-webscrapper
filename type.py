bccc74c29ba88bfe4deb8d5446d41e194c0c4611
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-15 11:15:03 +0900

    [2.2] Tuples and Dicts

--- type.py
+++ type.py
@@ -5,17 +5,34 @@ a_boolean = False
 a_none = None
 
 b_list = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+b_tuple = ("Mon", "Tue", "Wed", "Thur", "Fri")
 
-print(type(a_string))
-print(type(a_number))
-print(type(a_float))
-print(type(a_boolean))
-print(type(a_none))
+nico = {
+    "name":"Nico",
+    "age": 29,
+    "korean": True,
+    "fav_food": ["Kimchi", "Sashimi"]
+}
 
-print(type(b_list), b_list, "Mon" in b_list, b_list[3], len(b_list))
+print(type(nico))
+print(nico)
+print(nico["name"])
 
-b_list.append("Sat")
-print(b_list)
+nico["handsome"] = True
+print(nico)
 
-b_list.reverse()
-print(b_list)
+# print(type(b_tuple))
+
+# print(type(a_string))
+# print(type(a_number))
+# print(type(a_float))
+# print(type(a_boolean))
+# print(type(a_none))
+
+# print(type(b_list), b_list, "Mon" in b_list, b_list[3], len(b_list))
+
+# b_list.append("Sat")
+# print(b_list)
+
+# b_list.reverse()
+# print(b_list)
