9b75b71da609be133bfb3ef83d74041d8dad71f3
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-15 11:42:13 +0900

    [2.5] Function Arguments

--- def.py
+++ def.py
@@ -1,10 +1,16 @@
-def say_hello():
-    print("Hello")
-    print("Bye")
+def say_hello(who="anonymous"):
+    print("Hello", who)
 
-say_hello()
-say_hello()
-say_hello()
+say_hello("NICO")
 say_hello()
 
-print(type(say_hello))
+def plus(a,b):
+    return a + b
+
+
+def minus(a,b=0):
+    return a - b
+
+print(plus(3,5))
+print(minus(3,5))
+print(minus(3))
