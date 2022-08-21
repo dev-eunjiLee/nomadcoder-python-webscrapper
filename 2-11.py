021227eb28391011879650e5011acfedab72f8f5
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-21 23:00:38 +0900

    [2-11] for in

---
+++ 2-11.py
@@ -0,0 +1,10 @@
+days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+
+for per_day in days:
+    if per_day == "Wed":
+        break
+    else:
+        print(per_day)
+
+for per_char in "python":
+    print(per_char)
