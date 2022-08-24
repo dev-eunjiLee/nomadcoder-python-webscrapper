e1357f90d9ddf01c0f14cb0d14164991557c2c15
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-24 19:04:50 +0900

    [2022 - 5.2] Initial Request

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import requests
-import beautifulsoup
+from requests import get
+from bs4 import BeautifulSoup
 
-url = "https://www.jobkorea.co.kr/Search/?stext=%ED%8C%8C%EC%9D%B4%EC%8D%AC"
 
-payload = {}
-headers = {
-    'Cookie': 'GTMVars=63787639f127dd435acf409c37584ef6; GTMVarsFrom=NET:23:29:14; jobkorea=Site_Oem_Code=C1; sm=keyword=%ed%8c%8c%ec%9d%b4%ec%8d%ac; ASP.NET_SessionId=iciupylo343lpverctrfpjgm'
-}
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+response = get(f"{base_url}{search_term}")
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    print(response.text)
+    res = BeautifulSoup(response.text)
+    print(1)
