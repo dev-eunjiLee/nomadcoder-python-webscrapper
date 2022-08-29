c0cf2a5f6cb73533a79cec55c28ff2ad6cc87996
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-29 23:36:00 +0900

    [5.5] Job Posts

--- main.py
+++ main.py
@@ -10,5 +10,11 @@ if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_="jobs")
-    print(1)
+    jobs = soup.find_all(name='section', class_="jobs")
+    print(len(jobs))
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop(-1)  # 마지막 항목 날리기
+        for post in job_posts:
+            print(post)
+            print("////////////////////")
